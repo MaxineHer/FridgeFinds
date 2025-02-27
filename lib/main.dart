018967c8d18c93cffd5651ee030deb70315c3ff5
stan964 <stan964@uowmail.edu.au>
2025-02-27 16:47:03 +0400

    firebase configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'adduser.dart';
 import 'fridgecon1.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -213,3 +219,4 @@ class BottomNavBar extends StatelessWidget {
     );
   }
 }
+
